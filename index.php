b581a22cc0477de1e613ba45efe031c3872a0f0a
Mridul Markandey <60755738+MridulMarkandey1999@users.noreply.github.com>
2020-06-15 06:30:48 +0530

    commit-3

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
 <body>
 
-<h2> My First Task </h2>
+  <h1><center> My First Task </center></h1>
 
 <br>
-<p> Hello everyone. This is Mridul Markandey. </p>
-<br><br>
-<h4> This image is coming from Cloud Front </h4>
+<p> Hello everyone. This is Mridul Markandey. I have successfully completed my first task under Hybrid Multi Cloud training. </p>
+<br>
+<h3> This image is coming from Cloud Front ....!!! </h3>
 <?php
 
 echo "The Earth <br>";
